94ab29c1f36d333f6a6ba6785b7aa1bb961a5495
paulVera2001 <pveramolla@gmail.com>
2025-02-14 22:01:49 -0500

    Flask usa el puerto por defecto 5000

--- app.py
+++ app.py
@@ -118,7 +118,8 @@ def borrar_historial():
     return jsonify({"message": "Historial eliminado correctamente"}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Render asigna un puerto dinámico
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Ejecutando en el puerto: {port}")
     app.run(host="0.0.0.0", port=port, debug=True)
 
 #if __name__ == '__main__':
