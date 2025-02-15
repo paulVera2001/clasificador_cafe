23514e68808ffbe3820b4a9f3fdecd0f79d2a04a
paulVera2001 <pveramolla@gmail.com>
2025-02-14 22:24:35 -0500

    carga unica del modelo

--- models/modelo_clasificador.py
+++ models/modelo_clasificador.py
@@ -1,15 +1,17 @@
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 import numpy as np
 from keras.utils import load_img, img_to_array
 
-# Ruta del modelo entrenado
-MODEL_PATH = "models/Modelo_clasificador_cafe_cnn_camPro_117a500muestrasParaCadaDefecto.h5"
-
-# Cargar el modelo una sola vez al inicio para evitar recargarlo en cada solicitud
-modelo = tf.keras.models.load_model(MODEL_PATH)
-#print(modelo.summary())  # Verifica la estructura del modelo
+modelo = None  # Evita cargarlo al inicio
 
 def predecir_clase(imagen_path):
+    global modelo
+    if modelo is None:  # Cargar solo si aún no se ha cargado
+        # Cargar el modelo una sola vez al inicio para evitar recargarlo en cada solicitud
+        modelo = load_model("models/Modelo_clasificador_cafe_cnn_camPro_117a500muestrasParaCadaDefecto.h5")
+        #print(modelo.summary())  
+        
     """Carga la imagen y la pasa por el modelo para hacer la clasificación."""
     imagen = load_img(imagen_path, target_size=(200, 200))
     imagen_array = img_to_array(imagen)
